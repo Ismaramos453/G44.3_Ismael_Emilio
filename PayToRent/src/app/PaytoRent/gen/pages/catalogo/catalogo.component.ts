e6c9334d153d0831d4f050d798a04c7a743d195e
Ismaramos453 <ismaramos453@gmail.com>
2023-04-24 11:06:50 +0100

    Servicios

--- PayToRent/src/app/PaytoRent/gen/pages/catalogo/catalogo.component.ts
+++ PayToRent/src/app/PaytoRent/gen/pages/catalogo/catalogo.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./catalogo.component.css']
 })
 export class CatalogoComponent {
+  ventanaAbierta: boolean = false;
+  precio: number = 500;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  abrirVentana() {
+    this.ventanaAbierta = true;
+  }
+
+  cerrarVentana() {
+    this.ventanaAbierta = false;
+  }
+
+  confirmarPrecio() {
+    // Aquí puedes agregar la lógica para confirmar el precio seleccionado.
+    console.log('Precio confirmado:', this.precio);
+    this.cerrarVentana();
+  }
+
+  onPriceChange(event: Event) {
+    this.precio = (event.target as HTMLInputElement).valueAsNumber;
+  }
 
 }

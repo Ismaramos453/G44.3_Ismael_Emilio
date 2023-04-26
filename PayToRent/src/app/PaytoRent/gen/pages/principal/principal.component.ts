248b8831b9c6aca99a78039613401aeca9e39306
EmilioDeniz <welidrams112@gmail.com>
2023-04-26 09:37:19 +0100

    Data Binding en menú principal

--- PayToRent/src/app/PaytoRent/gen/pages/principal/principal.component.ts
+++ PayToRent/src/app/PaytoRent/gen/pages/principal/principal.component.ts
@@ -9,10 +9,12 @@ import { Product, ProductExchangerService } from 'src/app/PaytoRent/services/pro
 })
 export class PrincipalComponent {
 
-  products!:Product[]
+  productsCarrousel!:Product[][]
+  isActive!:number
 
   ngOnInit(){
-    this.products = this.productService.products
+    this.productsCarrousel = this.divideLista(this.productService.products)
+    this.isActive=0
   }
   
   constructor(private router: Router, private productService:ProductExchangerService) {
@@ -24,5 +26,13 @@ export class PrincipalComponent {
 
   llamadaProducto(){
     this.router.navigate(['./producto']);
+  }
+
+  divideLista(list:Product[]) :Product[][]{
+    const subListas: Product[][] = [];
+    for (let i = 0; i < list.length; i += 4) {
+      subListas.push(list.slice(i, i + 4));
+    }
+    return subListas;
   }
 }

c10c77031a43b435adbc99b64620c73610814b04
Ismaramos453 <ismaramos453@gmail.com>
2023-05-15 10:43:34 +0100

    Ionic

--- PayToRentMobile/src/app/PaytoRent/services/product.service.ts
+++ PayToRentMobile/src/app/PaytoRent/services/product.service.ts
@@ -1,9 +1,94 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { combineLatest } from 'rxjs';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface Product {
+  name: string;
+  price: string;
+  colour: string;
+  description: string;
+  owner: string;
+  rating: number;
+  image: string;
+  seats: string;
+  id: string;
+  comments: string[];
+  gallery: string[];
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  products: Observable<Product[]>;
+
+  constructor(private firestore: AngularFirestore) {
+    const coches$ = this.firestore.collection<Product>('Coches').valueChanges();
+    const motos$ = this.firestore.collection<Product>('Motos').valueChanges();
+    const camiones$ = this.firestore.collection<Product>('Camiones').valueChanges();
+    const bicicletas$ = this.firestore.collection<Product>('Bicicletas').valueChanges();
+    const skates$ = this.firestore.collection<Product>('Skates').valueChanges();
+    const patinetes$ = this.firestore.collection<Product>('Patinete').valueChanges();
+  
+    this.products = combineLatest([coches$, motos$, camiones$, bicicletas$, skates$, patinetes$]).pipe(
+      map(products => products.reduce((accumulator, currentValue) => accumulator.concat(currentValue)))
+    );
+  }
+  
+  getProductsByColor(color: string): Observable<Product[]> {
+    return this.products.pipe(
+      map(products => products.filter(product => product.colour.toLowerCase() === color.toLowerCase()))
+    );
+  }
+
+
+
+  getMainImagesFromCollections(): Observable<string[]> {
+    const coches$ = this.firestore.collection<Product>('Coches').valueChanges();
+    const motos$ = this.firestore.collection<Product>('Motos').valueChanges();
+    const camiones$ = this.firestore.collection<Product>('Camiones').valueChanges();
+    const bicicletas$ = this.firestore.collection<Product>('Bicicletas').valueChanges();
+    const skates$ = this.firestore.collection<Product>('Skates').valueChanges();
+    const patinetes$ = this.firestore.collection<Product>('Patinete').valueChanges();
+    
+    return combineLatest([coches$, motos$, camiones$, bicicletas$, skates$, patinetes$]).pipe(
+      map(products => products.reduce((accumulator, currentValue) => accumulator.concat(currentValue))),
+      map(products => products.map(product => product.image))
+    );
+  }
+
+  // En ProductService
+setCollection(collectionName: string) {
+  this.products = this.firestore.collection<Product>(collectionName).valueChanges();
 }
+  
+
+  getProductsBySeats(seats: string): Observable<Product[]> {
+    return this.products.pipe(
+      map(products => products.filter(product => product.seats === seats))
+    );
+  }
+  getProductsByPrice(price: number): Observable<Product[]> {
+    return this.products.pipe(
+      map(products => products.filter(product => parseInt(product.price) <= price))
+    );
+  }
+  getProductById(id: string): Observable<Product | undefined> {
+    return this.products.pipe(
+      map(products => products.find(product => product.id === id))
+    );
+  }
+
+  getProductsByCollection(collection: string): Observable<Product[]> {
+    return this.firestore.collection<Product>(collection).valueChanges();
+  }
+  
+getProductsFromCollection(collectionName: string): Observable<Product[]> {
+  return this.firestore.collection<Product>(collectionName).valueChanges();
+}
+
+}
+
